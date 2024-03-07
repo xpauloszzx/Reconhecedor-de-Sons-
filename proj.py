86d61357712ed012dae9aa54a0fcdb4b213256dc
Paulinho Souza <82713803+xpauloszzx@users.noreply.github.com>
2024-03-07 15:28:42 -0300

    Separando os arquivos de audio de sirene para extrair caracteristicas posteriormente

--- proj.py
+++ proj.py
@@ -1,8 +1,11 @@
 import wave
 import os
+import shutil
+import os
+
 
 # Diretório contendo os arquivos de áudio .wav
-diretorio =  r'D:\Users\psgs\Documents\audio'
+diretorio =  r'D:\Users\psgs\Documents\audio'  #alterar isso pq estavamos no cin
 
 # Lista para armazenar os arquivos de áudio abertos
 audios = []
@@ -26,3 +29,26 @@ for nome_arquivo, audio in audios:
     audio.close()
 print(i)
 print(audios)
+
+# -----------
+
+# Diretório contendo os arquivos de áudio .wav originais
+diretorio_origem = r'D:\Users\aab2\Documents\audio' #alterar isso pq estavamos no cin
+
+# Diretório onde os arquivos selecionados serão copiados
+diretorio_destino = r'D:\Users\aab2\Documents\copiados' #alterar isso pq estavamos no cin
+
+# Criar o diretório de destino se ele não existir
+if not os.path.exists(diretorio_destino):
+    os.makedirs(diretorio_destino)
+
+# Lista dos arquivos selecionados
+arquivos_selecionados = [arquivo for arquivo in os.listdir(diretorio_origem) if arquivo.endswith('.wav') and '-42.wav' in arquivo]
+
+# Copiar os arquivos selecionados para o diretório de destino
+for arquivo in arquivos_selecionados:
+    caminho_origem = os.path.join(diretorio_origem, arquivo)
+    caminho_destino = os.path.join(diretorio_destino, arquivo)
+    shutil.copy2(caminho_origem, caminho_destino)
+
+print("Arquivos copiados com sucesso para:", diretorio_destino)
